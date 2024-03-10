c809a282c62d5d1b2727d6e995f2e4943e52d24e
Jakub Pacler <jakub.pacler@gmail.com>
2024-03-10 11:00:37 +0100

    Add project files.

---
+++ ContactBook/Data/ContactBookContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBook.Data
+{
+    public class ContactBookContext : DbContext
+    {
+        public ContactBookContext(DbContextOptions<ContactBookContext> options) 
+            : base(options) 
+        {
+        }
+        public DbSet<Contact>Contacts { get; set; }
+    }
+}
---
+++ ContactBook/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContactBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        // Imię i nazwisko kontaktu
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // Numer telefonu kontaktu
+        [Required]
+        [StringLength(20)]
+        public string TelephoneNumber { get; set; }
+
+        // Miasto zamieszkania kontaktu
+        [Required]
+        [StringLength(50)]
+        public string City { get; set; }
+    }
+}
